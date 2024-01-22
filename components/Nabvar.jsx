8a0800a5342be45c815cba7e5608d57d0e08ff63
furkanimir <frk.xw7@gmail.com>
2024-01-22 17:58:29 +0300

    Update Nabvar.jsx

--- components/Nabvar.jsx
+++ components/Nabvar.jsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 export default function Navbar() {
 
     return(
-        <nav className="flex justify-between items-center bg-slate-500 px-8 py-5">
+        <nav className="flex justify-between items-center bg-gradient-to-tr from-slate-500 to-blue-500 px-8 py-5">
             <Link className="text-white font-bold" href={"/"}>Home</Link>
-            <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
+            <Link className="bg-gradient-to-br from-green-500 to-blue-300 text-white p-2" href={"/addTopic"}>Add Topic</Link>
+            {/* <Link className="bg-gradient-to-r from-purple-600 to-purple-700 text-white p-2" href={"/addTopic"}>Add Topic</Link> */}
         </nav>
     );
 }
